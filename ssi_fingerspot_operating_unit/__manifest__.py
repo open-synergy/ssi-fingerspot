59b8699922f4ea698693f4203614d451d224d1c4
Andhitia Rama <andhitia.r@gmail.com>
2026-07-18 07:45:25 +0700

    [ADD] ssi_fingerspot_operating_unit
    Modul glue baru yang menambahkan integrasi Operating Unit pada
    fingerspot.attendance.machine.batch lewat mixin.single_operating_unit,
    lengkap dengan security (res.groups + ir.rule) dan view (tree/form/
    search), mengikuti pola baku glue module Operating Unit SSI.

---
+++ ssi_fingerspot_operating_unit/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Fingerspot Attendance Machine Integration + Operating Unit",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_fingerspot",
+        "ssi_operating_unit_mixin",
+    ],
+    "data": [
+        "security/res_group/fingerspot_attendance_machine_batch.xml",
+        "security/ir_rule/fingerspot_attendance_machine_batch.xml",
+        "view/fingerspot_attendance_machine_batch.xml",
+    ],
+}
